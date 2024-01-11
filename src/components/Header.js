7e3b759896eeeb20a59910a168a22dd29c5473b3
datta934 <official.mauaa@gmail.com>
2024-01-11 09:05:57 +0530

    Landing page with login and signup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="absolute">
+      <img src="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2023.ico" alt="NETFLIX"  className="p-2"/>
+    </div>
+  );
+};
+
+export default Header;
